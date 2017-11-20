bf8338507250ec488c47a3e61dd842e57858d805
hailigu <dengshaodong@hailigu.com>
2017-11-20 16:02:26 +0800

    video write problem

--- code/ssd-zhiyuan/detect_video.py
+++ code/ssd-zhiyuan/detect_video.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 import os
 import math
 import random
@@ -13,6 +11,7 @@ slim = tf.contrib.slim
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import sys
+
 sys.path.append('../')
 
 from nets import ssd_vgg_300, ssd_common, np_methods
@@ -40,8 +39,8 @@ with slim.arg_scope(ssd_net.arg_scope(data_format=data_format)):
     predictions, localisations, _, _ = ssd_net.net(image_4d, is_training=False, reuse=reuse)
 
 # Restore SSD model.
-ckpt_filename = 'F:/Desktop/SSD-Tensorflow-master/SSD-Tensorflow-master/checkpoints/ssd_300_vgg.ckpt/ssd_300_vgg.ckpt' 
-#ckpt_filename = 'checkpoints/ssd_300_vgg.ckpt'
+ckpt_filename = 'C:/Users/Administrator/Desktop/SSD-Tensorflow-master/SSD-Tensorflow-master/checkpoints/ssd_300_vgg.ckpt/ssd_300_vgg.ckpt'
+# ckpt_filename = 'checkpoints/ssd_300_vgg.ckpt'
 isess.run(tf.global_variables_initializer())
 saver = tf.train.Saver()
 saver.restore(isess, ckpt_filename)
@@ -49,17 +48,18 @@ saver.restore(isess, ckpt_filename)
 # SSD default anchor boxes.
 ssd_anchors = ssd_net.anchors(net_shape)
 
+
 # Main image processing routine.
 def process_image(img, select_threshold=0.5, nms_threshold=.45, net_shape=(300, 300)):
     # Run SSD network.
     rimg, rpredictions, rlocalisations, rbbox_img = isess.run([image_4d, predictions, localisations, bbox_img],
                                                               feed_dict={img_input: img})
-    
+
     # Get classes and bboxes from the net outputs.
     rclasses, rscores, rbboxes = np_methods.ssd_bboxes_select(
-            rpredictions, rlocalisations, ssd_anchors,
-            select_threshold=select_threshold, img_shape=net_shape, num_classes=21, decode=True)
-    
+        rpredictions, rlocalisations, ssd_anchors,
+        select_threshold=select_threshold, img_shape=net_shape, num_classes=21, decode=True)
+
     rbboxes = np_methods.bboxes_clip(rbbox_img, rbboxes)
     rclasses, rscores, rbboxes = np_methods.bboxes_sort(rclasses, rscores, rbboxes, top_k=400)
     rclasses, rscores, rbboxes = np_methods.bboxes_nms(rclasses, rscores, rbboxes, nms_threshold=nms_threshold)
@@ -67,55 +67,41 @@ def process_image(img, select_threshold=0.5, nms_threshold=.45, net_shape=(300, 
     rbboxes = np_methods.bboxes_resize(rbbox_img, rbboxes)
     return rclasses, rscores, rbboxes
 
+
 def bboxes_draw_on_img(img, classes, scores, bboxes, color=[255, 0, 0], thickness=2):
     shape = img.shape
     for i in range(bboxes.shape[0]):
         bbox = bboxes[i]
-        #color = colors[classes[i]]
+        # color = colors[classes[i]]
         # Draw bounding box...
         p1 = (int(bbox[0] * shape[0]), int(bbox[1] * shape[1]))
         p2 = (int(bbox[2] * shape[0]), int(bbox[3] * shape[1]))
         cv2.rectangle(img, p1[::-1], p2[::-1], color, thickness)
         # Draw text...
         s = '%s/%.3f' % (classes[i], scores[i])
-        p1 = (p1[0]-5, p1[1])
+        p1 = (p1[0] - 5, p1[1])
         cv2.putText(img, s, p1[::-1], cv2.FONT_HERSHEY_DUPLEX, 0.4, color, 1)
+# 获得视频的格式
+cap = cv2.VideoCapture("C:/Users/Administrator/Desktop/SSD-Tensorflow-master/08_49_47.mp4")
 
-cap = cv2.VideoCapture("E:/igcity/demo/08_49_47.mp4") 
-#cap = cv2.VideoCapture(0)
-print(0)
-# Define the codec and create VideoWriter object
-#fourcc = cv2.cv.FOURCC(*'XVID')
-fourcc = cv2.VideoWriter_fourcc(*'XVID') 
+# 获得码率及尺寸
+fps = cap.get(cv2.CAP_PROP_FPS)
+size = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),
+        int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
 
-rval, frame = cap.read()
-h, w = frame.shape[:2]
-out = cv2.VideoWriter('E:/igcity/demo/output1.avi', fourcc, 20, (h,w))
-
-
-num=0
+# 指定写视频的格式, I420-avi, MJPG-mp4
+out = cv2.VideoWriter('out.avi',  cv2.VideoWriter_fourcc(*'XVID'), 20, (size),1)
 
 while cap.isOpened():
-    # get a frame
-    print(0)
-    rval, frame = cap.read()
-    # save a frame
-    if rval==True:
-      #  frame = cv2.flip(frame,0)
-        rclasses, rscores, rbboxes=process_image(frame)
+    success, frame = cap.read()
+    if success:
+        rclasses, rscores, rbboxes = process_image(frame)
         bboxes_draw_on_img(frame,rclasses,rscores,rbboxes)
-        print(rclasses)
-        out.write(frame)
-        num=num+1
-        print(num)
-    else:
-        break
-    # show a frame
-    cv2.imshow("capture", frame)
+        # cv2.imshow("Oto Video", frame)  # 显示
+        out.write(frame)  # 写视频帧
+    success, frame = cap.read()  # 获取下一帧
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 cap.release()
 out.release()
 cv2.destroyAllWindows()
-
-
