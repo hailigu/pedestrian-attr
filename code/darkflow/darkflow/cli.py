5931fac1923c11d81d531a4c6e5cf8971ce2373f
Adesun <AvenSun@live.com>
2018-01-17 22:33:04 +0800

    last syncing

--- code/darkflow/darkflow/cli.py
+++ code/darkflow/darkflow/cli.py
@@ -12,22 +12,26 @@ def cliHandler(args):
         for d in dirs:
             this = os.path.abspath(os.path.join(os.path.curdir, d))
             if not os.path.exists(this): os.makedirs(this)
-    _get_dir([FLAGS.imgdir, FLAGS.binary, FLAGS.backup,
-             os.path.join(FLAGS.imgdir,'out'), FLAGS.summary])
+    
+    requiredDirectories = [FLAGS.imgdir, FLAGS.binary, FLAGS.backup, os.path.join(FLAGS.imgdir,'out')]
+    if FLAGS.summary:
+        requiredDirectories.append(FLAGS.summary)
+
+    _get_dir(requiredDirectories)
 
     # fix FLAGS.load to appropriate type
     try: FLAGS.load = int(FLAGS.load)
     except: pass
 
     tfnet = TFNet(FLAGS)
-
+    
     if FLAGS.demo:
         tfnet.camera()
         exit('Demo stopped, exit.')
 
     if FLAGS.train:
         print('Enter training ...'); tfnet.train()
-        if not FLAGS.savepb:
+        if not FLAGS.savepb: 
             exit('Training finished, exit.')
 
     if FLAGS.savepb:
